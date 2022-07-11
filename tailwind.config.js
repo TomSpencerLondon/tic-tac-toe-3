9bbfcfc28240f9d2be54148ab0bf0f1ac9b2d2fd
Tom <thomas.spencer@codurance.com>
2022-07-11 08:19:47 +0100

    edit config for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-const plugin = require('tailwindcss/plugin');
 module.exports = {
   content: ['./src/main/resources/templates/**/*.html'],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
+    extend: {},
+    minWidth: {
+      '0': '0',
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+      '4/5': '80%',
+      'full': '100%',
     },
   },
   plugins: [
