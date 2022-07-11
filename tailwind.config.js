086e1ca20736caed3160c780da7b8727c0247c14
Tom <thomas.spencer@codurance.com>
2022-07-11 08:40:43 +0100

    add tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,14 +13,6 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    plugin(function ({addBase, theme}) {
-      addBase({
-        'h1': {fontSize: theme('fontSize.2xl')},
-        'h2': {fontSize: theme('fontSize.xl')},
-        'h3': {fontSize: theme('fontSize.lg')},
-        'ol,ul': {listStyleType: 'revert'}
-      })
-    })
+    require('@tailwindcss/typography')
   ],
 };
